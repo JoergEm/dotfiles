a76ff91fbd7147de1c31e06ccc240c124b18d6eb
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:15:04 +0100

    Dotfiles Vim Python Template

---
+++ vim/templates/skeleton.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+FILENAME.py
+Copyright DATUM INHABER
+@GitHub JoergEm
+This program is WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+"""
+
+
+START
+
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 2 and sys.argv[1] == '--help':
+        print(__doc__)
+        sys.exit(main(sys.argv))
