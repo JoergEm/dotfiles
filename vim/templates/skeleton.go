ec8c63d4879145ac29f08925a30335c6016ff7a4
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:13:17 +0100

    Dotfiles Vim Go Template

---
+++ vim/templates/skeleton.go
@@ -0,0 +1,20 @@
+//
+// FILENAME.go
+// 
+// Copyright DATUM INHABER
+// @GitHub JoergEm
+//
+// This program is WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// 
+
+
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main() { START
+}
