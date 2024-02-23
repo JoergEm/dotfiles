3a89e03f7971609f60e4fe4e02e5911c65edbef3
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:14:35 +0100

    Dotfiles Vim JavaScript Template

---
+++ vim/templates/skeleton.js
@@ -0,0 +1,13 @@
+/*
+ * FILENAME.js
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+
+START
