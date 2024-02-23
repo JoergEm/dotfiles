8faa300b5d5b1b54d6049720e06a49dda2a36a27
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:10:59 +0100

    Dotfiles Vim Cobol Template

---
+++ vim/templates/skeleton.cbl
@@ -0,0 +1,16 @@
+       $ SET SOURCEFORMAT"FREE"
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FILENAME.
+       AUTHOR. INHABER.
+       INSTALLATION. @GitHub JoergEm.
+       DATE-WRITTEN. DATUM.
+       SECURITY.
+           **  This program is WITHOUT ANY WARRANTY;
+           **  without even the implied warranty of MERCHANTABILITY or
+           **  FITNESS FOR A PARTICULAR PURPOSE.
+
+       DATA DIVISION.
+       START
+
+       PROCEDURE DIVISION.
+           STOP RUN.
