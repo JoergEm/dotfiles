d952f3904f08a5e8f66b15f83af4dc3552d834d5
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:12:25 +0100

    Dotfiles Vim C# Template

---
+++ vim/templates/skeleton.cs
@@ -0,0 +1,21 @@
+/*
+ * FILENAME.cs
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+using System;
+
+class FILENAME
+{
+	
+    static void Main()
+    {
+		START
+	}
+}
