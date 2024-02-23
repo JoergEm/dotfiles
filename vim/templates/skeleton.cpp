2e4b68c33508d8bb493e34706d35f903dcab49cc
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:11:34 +0100

    Dotfiles Vim C++ Template

---
+++ vim/templates/skeleton.cpp
@@ -0,0 +1,22 @@
+/*
+ * FILENAME.cpp
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+
+#include <bits/stdc++.h>
+#include <iostream>
+#define mod 1000000007
+#define ll long long
+using namespce std;
+int main()
+{   
+    START
+    return 0;
+}
