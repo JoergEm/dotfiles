c651ba33fcb0ba15a7e7548e663bc87d493aa62f
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:15:26 +0100

    Dotfiles Vim R Template

---
+++ vim/templates/skeleton.R
@@ -0,0 +1,11 @@
+#
+# FILENAME.R
+# 
+# Copyright DATUM INHABER
+# @GitHub JoergEm
+# 
+# This program is WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+START
+if (!require('ggplot2')) install.packages('ggplot2'); library('ggplot2')
