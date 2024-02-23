04ded1a06e9ff7baa4f3e183b695545a70ed1cb6
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:14:08 +0100

    Dotfiles Vim Java Template

---
+++ vim/templates/skeleton.java
@@ -0,0 +1,20 @@
+/*
+ * FILENAME.java
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+
+
+public class FILENAME {
+	
+	public static void main (String[] args) {
+		START
+		
+	}
+}
