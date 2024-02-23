708b5c747162040c4e6d6db6fc5c5fd9b88eb8f3
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:12:52 +0100

    Dotfiles Vim Fortran Template

---
+++ vim/templates/skeleton.F90
@@ -0,0 +1,15 @@
+!
+! Copyright DATUM INHABER
+! @GitHub JoergEm
+! 
+! This program is WITHOUT ANY WARRANTY; without even the implied
+! warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+! 
+
+
+module FILENAMEModule
+
+   use iso_fortran_env
+    START
+
+end module
