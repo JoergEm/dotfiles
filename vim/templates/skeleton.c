677a7e0c52a120ad4e8e3df22e0145bc382b41a7
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-02-23 18:08:13 +0100

    Dotfiles Vim Templates Init

---
+++ vim/templates/skeleton.c
@@ -0,0 +1,18 @@
+//
+// FILENAME.c
+// 
+// Copyright DATUM INHABER
+// @GitHub JoergEm
+//
+// This program is WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    START
+   return 0;
+}
