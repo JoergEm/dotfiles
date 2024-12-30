ee4966c368935e04aa677b58ef43ebb767c8a1c9
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-12-30 17:51:06 +0000

    Terraform Skeleton Init

---
+++ vim/templates/skeleton.tf
@@ -0,0 +1,30 @@
+/*
+ * main.tf
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "<PROJECT_ID>"
+  region  = ""
+  zone    = ""
+}
+
+resource "" {
+  name = ""
+}
