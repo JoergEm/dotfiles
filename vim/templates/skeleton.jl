24458c4be8b6f800825a4decd9c38da99a1ed95d
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-12-30 17:26:47 +0000

    Julia Skeleton Init

---
+++ vim/templates/skeleton.jl
@@ -0,0 +1,14 @@
+#=
+FILENAME.jl
+Copyright DATUM INHABER
+@GitHub JoergEm
+
+This program is WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+=#
+
+function START()
+    return
+end
+
+println()
