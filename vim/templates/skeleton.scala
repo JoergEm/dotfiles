c6f08266bf2a7257ace217c9fce399eb4ab35f4e
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2024-12-30 17:44:05 +0000

    Scala Skeleton Init

---
+++ vim/templates/skeleton.scala
@@ -0,0 +1,18 @@
+//> using scala 3.5.2
+
+/**
+ * FILENAME.scala
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+
+ @main
+def main() =
+    START
+    println()
