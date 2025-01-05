2cd095fe394e999b979170d182ed3f18967a5d45
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-01-05 10:55:17 +0000

    Kotlin Skeleton Init

---
+++ vim/templates/skeleton.kt
@@ -0,0 +1,15 @@
+/*
+ * FILENAME.java
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+
+ fun main() {
+    START
+ }
