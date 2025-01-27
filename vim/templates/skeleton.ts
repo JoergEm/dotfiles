04728decb0a8bfc49a39f1ef4b811311c2b949fd
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-01-27 21:29:21 +0000

    Typescript Skeleton Init

---
+++ vim/templates/skeleton.ts
@@ -0,0 +1,13 @@
+/*
+ * FILENAME.ts
+ * 
+ * Copyright DATUM INHABER
+ * @GitHub JoergEm
+ * 
+ * This program is WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * 
+ */
+
+START
