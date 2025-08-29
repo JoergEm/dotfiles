016aea7eddf4ce60d80429ed5917b8071df0afd9
elvishasleftyahoo <elvishasleftyahoo@googlemail.com>
2025-08-29 18:44:46 +0200

    Skeleton Removed BadWhiteSpace

--- .vim/templates/skeleton.cpp
+++ .vim/templates/skeleton.cpp
@@ -1,21 +1,21 @@
-/* 
+/*
  * VERSION
  * FILENAME.cpp
- * 
+ *
  * Copyright DATUM INHABER
  * @GitHub JoergEm
- * 
+ *
  * This program is WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * 
+ *
+ *
  */
 
 #include <iostream>
 #include <string>
 using namespace std;
 int main()
-{   
+{
     START
     return 0;
 }
