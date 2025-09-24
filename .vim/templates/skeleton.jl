0a2d0cfc506dbb0a9b72dd335c113386d891970c
elvishasleftyahoo <elvishasleftyahoo@googlemail.com>
2025-09-24 22:52:13 +0200

    Julia Skeleton Main Args Function Improvememnt

--- .vim/templates/skeleton.jl
+++ .vim/templates/skeleton.jl
@@ -8,7 +8,8 @@ This program is WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 =#
 
-function START()
+function (main)(args)
+    START
     return
 end
 
