7d8738a70bab491f938da9eb35ee1c6c3cacadb9
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-12-11 18:23:38 +0100

    [Fix] Empty main call but still sys.argv

--- .vim/templates/skeleton.py
+++ .vim/templates/skeleton.py
@@ -14,12 +14,14 @@ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 START
 
 
-def main(args):
+def main():
     return 0
 
 
 if __name__ == '__main__':
+    main()
     import sys
     if len(sys.argv) == 2 and sys.argv[1] == '--help':
         print(__doc__)
-        sys.exit(main(sys.argv))
+        sys.exit(0)
+    sys.exit(main())
