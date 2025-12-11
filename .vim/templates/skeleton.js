136de7f9edecb4f5ca08428100c6382b00461dc1
Jörg Mekka <elvishasleftyahoo@googlemail.com>
2025-12-11 18:25:27 +0100

    [Fix] Whitespace

--- .vim/templates/skeleton.js
+++ .vim/templates/skeleton.js
@@ -1,15 +1,15 @@
-/* 
+/*
  *
  * node VERSION
  * FILENAME.js
- * 
+ *
  * Copyright DATUM INHABER
  * @GitHub JoergEm
- * 
+ *
  * This program is WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * 
+ *
+ *
  */
 
 START
